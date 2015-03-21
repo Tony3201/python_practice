ebff7234198876c27fa82367235d787ee3ca1653
leo881205 <lisong1205@126.com>
2015-03-21 16:55:31 +0800

    modified:   update_hosts/update_hosts.py

--- update_hosts/update_hosts.py
+++ update_hosts/update_hosts.py
@@ -4,6 +4,12 @@ import urllib
 import os
 import platform
 
+'''
+TBD
+1)backup the old hosts and update the hosts, and append it
+2)ping google server whether to update hosts(use ping or ouropen)
+3)can not connect the hosts_server,exit the script
+'''
 def gethtml(url):
     page = urllib.urlopen(url)
     html = page.read()

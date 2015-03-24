b1168bcd6b3d43d8305ffc660347363e9b137e4d
leo881205 <lisong1205@126.com>
2015-03-24 12:59:00 +0800

    modified:   update_hosts.py

--- update_hosts/update_hosts.py
+++ update_hosts/update_hosts.py
@@ -9,6 +9,7 @@ TBD
 1)backup the old hosts and update the hosts, and append it
 2)ping google server whether to update hosts(use ping or ouropen)
 3)can not connect the hosts_server,exit the script
+4)have not permission to wirte this hosts
 '''
 def gethtml(url):
     page = urllib.urlopen(url)

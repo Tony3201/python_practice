b85560ad234516d7b6d136b44947a54247b676c3
songleo <lisong1205@126.com>
2016-06-07 14:57:52 +0800

    auto commit

--- demo/get_human_readable_mem.py
+++ demo/get_human_readable_mem.py
@@ -2,26 +2,38 @@
 # coding:utf8
 
 
-# def sizeof_fmt(num, suffix='B'):
-#     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-#         if abs(num) < 1024.0:
-#             return "%3.1f%s%s" % (num, unit, suffix)
-#         num /= 1024.0
-#     return "%.1f%s%s" % (num, 'Yi', suffix)
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
 
 def human_readable_data_quantity(quantity, multiple=1024):
-    quantity = abs(quantity)
-    for unit in ['M', 'G', 'T', 'P', 'E', 'Z']:
-        # if quantity < 10G, unit is M
-        if quantity < multiple * 10:
-            return "%d%s" % (quantity, unit)
-        quantity /= multiple
+    try:
+        abs_quantity = abs(quantity)
+        for unit in ['M', 'G', 'T', 'P', 'E', 'Z']:
+            # if quantity < 10G, unit is M
+            if abs_quantity < multiple * 10:
+                return "%d%s" % (abs_quantity, unit)
+            abs_quantity /= multiple
 
-    return "%d%s" % (quantity, 'Y')
+        return "%d%s" % (abs_quantity, 'Y')
+
+    except TypeError:
+        return '0M'
 
 
-print human_readable_data_quantity(10240)
+print human_readable_data_quantity('a')
 print human_readable_data_quantity(20480)
 print human_readable_data_quantity(20479)
+print human_readable_data_quantity(-2)
 print human_readable_data_quantity(19456)
 
+
+# print sizeof_fmt('a')
+print sizeof_fmt(20480)
+print sizeof_fmt(20479)
+print sizeof_fmt(-2)
+print sizeof_fmt(19456)

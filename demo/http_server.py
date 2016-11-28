0f682c4064d75f3b9016cc69a0c020397078bf8c
songleo <lisong1205@126.com>
2016-11-28 13:30:56 +0800

    auto commit

---
+++ demo/http_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding:utf8
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8888
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()

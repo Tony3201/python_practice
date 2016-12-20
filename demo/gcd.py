2d2e04e684c9f1c006a464fe354a7848e7d2bb5b
songleo <lisong1205@126.com>
2016-12-20 10:14:10 +0800

    auto commit

---
+++ demo/gcd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf8
+
+
+# python 2.7.6
+
+def gcd(x, y):
+    if y == 0:
+        print 'x=%d' % (x)
+        return x
+    else:
+        print 'x = %d, y = %d, x mod y = %d' % (x, y, x % y)
+        return gcd(y, x % y)
+
+
+print 'gcd(24, 60) = %d' % (gcd(24, 60))

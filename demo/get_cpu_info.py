0e9eae9ab7f339736772fb34538b0175469d6d7a
songleo <lisong1205@126.com>
2017-01-16 11:54:28 +0800

    auto commit

--- demo/get_cpu_info.py
+++ demo/get_cpu_info.py
@@ -5,11 +5,11 @@ import psutil
 
 logical_cores = psutil.cpu_count(logical=True)
 if logical_cores is None:
-	logical_cores = 1
+    logical_cores = 1
 
 physical_cores = psutil.cpu_count(logical=False)
 if physical_cores is None:
-	physical_cores = 1
+    physical_cores = 1
 
 print "logical_cores: %d" % (logical_cores)
 print "physical_cores: %d" % (physical_cores)

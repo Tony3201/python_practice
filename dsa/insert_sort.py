4b78f36b409a5d864d7f2dd668af24d428aa501b
songleo <lisong1205@126.com>
2017-01-18 15:42:50 +0800

    auto commit

---
+++ dsa/insert_sort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf8
+
+
+def insertSort(array, size):
+    for i in xrange(2, size):
+        key = array[i]
+        for j in xrange(0, i - 1):
+            if array[]
+
+
+if __name__ == "__main__":
+    arr = [3, 1, 6, 9, 0]
+    print arr
+    arr = insertSort(arr, 5)
+    print arr

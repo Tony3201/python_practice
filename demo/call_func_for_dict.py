5305e57647093fdc2ce1524bc86a8209ad0082c2
songleo <lisong1205@126.com>
2017-07-19 10:15:26 +0800

    auto commit

---
+++ demo/call_func_for_dict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+def modify(dict):
+    dict['0'] = 0
+    dict['1'] = 100000000000000
+    dict['2'] = 20000000008880
+
+
+dict = {'0': 1, '1': 2, '2': 0}
+
+print dict
+
+modify(dict)
+
+print dict

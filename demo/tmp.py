5e8cbf28906ceeb0569879bc072b8e59227c89b8
songleo <lisong1205@126.com>
2017-08-15 17:52:21 +0800

    auto commit

--- demo/tmp.py
+++ demo/tmp.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python
 
+jobid = [1, 2, 3, 4]
+arrjobid = [2, 5, 6, 7]
+jobarryidx = [2, 9, 10, 11]
 
-empty_str = ""
+if (i for i in jobid if i not in arrjobid):
+    unexistJobid = [
+        i for i in jobid if i not in arrjobid and i not in jobarryidx]
 
-if empty_str:
-    print 'if empty_str is True'
-else:
-    print 'if empty_str is False'
-
-
-int_0 = 0
-
-if int_0:
-    print 'if int_0 is True'
-else:
-    print 'if int_0 is False'
-
-arrayjob.jobName = arrayjob.jobName[:len(arrayjob.jobName) - 1] \
-    + '-' + str(replydata.get('ArrIdxSlice')[-1]) + ']'
+print unexistJobid
